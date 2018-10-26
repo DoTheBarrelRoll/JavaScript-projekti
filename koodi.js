fadd5c4207510e631d2272e2eb8fe1ade1c2ab05
DoTheBarrelRoll <32955643+DoTheBarrelRoll@users.noreply.github.com>
2018-10-26 16:26:42 +0300

    Lisätty toiminnallisuutta
    - Verkkosivu muistaa edellisellä istunnolla luodut tehtävät
    - Lisätty nappi, jolla voi poistaa kaikki valmiit tehtävät

--- koodi.js
+++ koodi.js
@@ -1,8 +1,6 @@
 //Miikka Niemeläinen ja Teemu Rossi, 2018, All rights reserved
 
 
-//Alustetaan muuttuja i, jolla saadaan yksilöllinen id jokaiselle todo listan objektille
-var i = 0;
 
 //funktio jolla muutetaan inputfieldin borderin väri  (jos tullut virhetilanne ei tämä jää punaiseksi kun alkaa korjaamaan virhettä eli kirjoittamaan inputtiin)
 function normaalivari() {
@@ -14,8 +12,7 @@ function normaalivari() {
 //funktio, jolla lisätään esineitä todo listalle
 function lisääTehtävä() {
 
-  //haetaan lisättävän tehtävän nimi tekstikentästä
-  //samalla tarkistellaan että on annettu jokin tehtävä
+  //haetaan lisättävän tehtävän nimi ja kuvaus tekstikentistä
   var lomake = document.forms["myForm"]["todoNimi"].value;
   var kuvaus = document.forms["myForm"]["todoKuvaus"].value;
   //tarkistetaan että on annettu jotain sisältöä tehtävään ja jos ei, annetaan alert+highlightataan kohdan
@@ -25,21 +22,22 @@ function lisääTehtävä() {
     x.style.borderColor = "red";
   }
   else {
-  //haetaan HTML tägi, johon tehtävä lisätään
+  //haetaan ul tägi, johon tehtävä lisätään
   var lisäys = document.getElementById("kesken");
 
 
-  //lisätään objekti sivulle checkboxin kera, jolla tehtävä saadaan poistettua kun se on valmis
+  //lisätään tehtävä listaan list iteminä, muotoillaan se bootstrapillä
+  // ja lisätään siihen nappi, jolla se saadaan siirrettyä valmiisiin tehtääviin
   lisäys.innerHTML += `<li>
                           <div class="card-body">
                             <h5 class="card-title">` + lomake + `</h5>
                             <p class="card-text">` + kuvaus + `</p>
-                            <button class="btn btn-primary btn-sm" id="objekti" onclick="poista(this)">Done</button>
+                            <button class="btn btn-primary btn-sm" onclick="poista(this)">Done</button>
                           </div>
                         </li>
                         `
 
-  //Tyhjennetään tekstikenttä
+  //Tyhjennetään tekstikentät
   document.forms["myForm"]["todoNimi"].value = "";
   document.forms["myForm"]["todoKuvaus"].value = "";
 }
@@ -52,7 +50,7 @@ function nakyvyys(){
 //haetaan elementit joiden sisältöä muutetaan
   var x = document.getElementById("valmiit");
   var y = document.getElementById("poistanappi");
-  // if lauseen avulla looppi jossa tarkistetaan mikä display style on käytössä
+  // if lauseen avulla tarkistetaan mikä display style on käytössä
   // ja muutetaan se vastakkaiseksi
   // samalla myös muutetaan näppäimen nimi (y variablen arvo) sen mukaan mitä näppäin sillä hetkellä tekee
   if(x.style.display == "none"){
@@ -68,14 +66,13 @@ function nakyvyys(){
 
 
 
-//Tällä funktiolla poistetaan valmiit tehtävät todo listasta
+//Tällä funktiolla siirretään valmis tehtävä toiseen tauluun
 
 function poista(btn) {
-
-  //Haetaan napin isänode (li)
+  //Haetaan napin parentnode (li)
   var task = btn.parentNode;
 
-  //Haetaan li:n isänode (ul)
+  //Haetaan li:n parentnode (ul)
   var ul = task.parentNode;
 
     //samalla tuodaan näkyviin näppäin
@@ -96,5 +93,42 @@ function poista(btn) {
   task.style.color = "gray";
   task.style.textDecoration = "line-through";
   task.childNodes[5].style.display = "none";
+  tallennus();
+}
+
+//tallennetaan valmiit ja keskeneräiset tehtävät localStorageen
+function tallennus() {
+  //haetaan keskeneräiset tehtävät ul elementistä ja lisätään ne localStorageen
+  var keskeneräiset = document.getElementById("kesken").innerHTML;
+  localStorage.setItem("lista1", keskeneräiset);
+
+  //haetaan valmiit tehtävät ul elementistä ja lisätään ne localStorageen
+  var valmiit = document.getElementById("valmiit").innerHTML;
+  localStorage.setItem("lista2", valmiit);
+}
+
+//Tarkistetaan, onko localStoragessa tallennettuja tehtäviä
+//Jos on, lisätään ne tauluihin
+function lataus() {
+  var keskeneräiset = document.getElementById("kesken");
+  keskeneräiset.innerHTML = localStorage.getItem("lista1");
+  var valmiit = document.getElementById("valmiit")
+  valmiit.innerHTML = localStorage.getItem("lista2");
+
+  //Tarkistetaan, onko valmiit taulussa tehtäviä
+  //Jos on, näytetään "Piilota valmiit" nappi
+  if (valmiit.children.length > 0) {
+    var x = document.getElementById('poistanappi').style.display = "block";
+  }
+}
+
+function poistavalmiit() {
+  var varmistus = confirm("Haluatko varmasti poistaa kaikki valmiit tehtävät?");
+  if (varmistus == true) {
+    localStorage.setItem("lista2", "");
+    lataus();
+  } else {
+
+  }
 
 }
