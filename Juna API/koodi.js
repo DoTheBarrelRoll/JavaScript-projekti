d67ea82db5b47fa5173f2dd15aa12afca5977c26
DoTheBarrelRoll <32955643+DoTheBarrelRoll@users.noreply.github.com>
2018-12-09 20:25:45 +0200

    ulkonäkömuokkausta

--- Juna API/koodi.js
+++ Juna API/koodi.js
@@ -1,10 +1,12 @@
 var tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
 let aika = (new Date(Date.now() - tzoffset)).toISOString();
-var iterator = 20;
+
 
 function lataaAsemat() {
-    $('#error').hide();
-    $('.taulut').hide();
+  $('#error').hide();
+  $('.taulut').hide();
+  $('#lähijunat').hide();
+  $('#kaukojunat').hide();
 
 
 
@@ -19,7 +21,7 @@ function lataaAsemat() {
     }
   })
 
-  $('#otsikko').html(aika);
+
 }
 
 function lataaJunat() {
@@ -27,65 +29,64 @@ function lataaJunat() {
   $('#error').hide();
   $('#taulukeho1').html('');
   $('#taulukeho2').html('');
-  iterator = 20;
+  var iterator = 20;
+  var iterator2 = 20;
 
   var lähtöasema = $('#lähtö').val();
   var määränpää = $('#maali').val();
   var url = "https://rata.digitraffic.fi/api/v1/live-trains/station/" + lähtöasema + "/" + määränpää + "?startDate=" + aika + "&limit=1000";
 
-  $("#otsikko").html(url);
   $.ajax({
-    url: url,
-    dataType: 'json',
-    success: function(result) {
-      if (result.queryString != null) {
-        $('#error').show();
-        $('.taulut').fadeOut();
-      }
-      $('.taulut').fadeIn(1500);
+      url: url,
+      dataType: 'json',
+      success: function(result) {
+        if (result.queryString != null) {
+          $('#error').show();
 
-      for (var i = 0; i < 20; i++) {
+        }
 
-        for (var j = 0; j < result[i].timeTableRows.length; j++) {
-          if (result[i].timeTableRows[j].stationShortCode == lähtöasema && result[i].timeTableRows[j].type == "DEPARTURE" && result[i].commuterLineID != "") {
-            var saapumisaika = new Date(result[i].timeTableRows[j].scheduledTime);
-            $('#taulukeho1').append(`
-                      <tr>
-                        <td>` + result[i].commuterLineID + `</td>
-                        <td>` + result[i].timeTableRows[j].commercialTrack + `</td>
-                        <td>` + saapumisaika.toUTCString().slice(17,22) + `</td>
+        $('.taulut').show();
+
+
+
+
+        for (var i = 0; i < result.length; i++)
+          for (var j = 0; j < result[i].timeTableRows.length; j++) {
+
+            if (result[i].timeTableRows[j].stationShortCode == lähtöasema && result[i].timeTableRows[j].type == "DEPARTURE" && result[i].commuterLineID != "") {
+              var saapumisaika = new Date(result[i].timeTableRows[j].scheduledTime);
+              var peruna = `
+                          <tr>
+                            <td>` + result[i].commuterLineID + `</td>
+                            <td>` + result[i].timeTableRows[j].commercialTrack + `</td>
+                            <td>` + saapumisaika.toUTCString().slice(17, 22) + `</td>
+                            <td>
+                          `;
+
+            }
+            if (result[i].timeTableRows[j].stationShortCode == määränpää && result[i].timeTableRows[j].type == "ARRIVAL" && result[i].commuterLineID != "") {
+
+              var perilläaika = new Date(result[i].timeTableRows[j].scheduledTime);
+              console.log(perilläaika.toUTCString().slice(17, 22));
+              $('#taulukeho1').append(peruna + perilläaika.toUTCString().slice(17, 22) + `</td>
                       </tr>
-                      `)
+                    `);
+
+            }
+
           }
 
-        }
-
-
-
       }
-
-      for (var i = 0; i < result.length; i++) {
-        for (var k = 0; k < result[i].timeTableRows.length; k++) {
-          if (result[i].trainCategory=="Long-distance" && result[i].timeTableRows[k].stationShortCode == lähtöasema && result[i].timeTableRows[k].type == "DEPARTURE"){
-            var saapumisaika = new Date(result[i].timeTableRows[k].scheduledTime);
-            $('#taulukeho2').append(`
-              <tr>
-                <td>` + result[i].trainType + `</td>
-                <td>` + result[i].timeTableRows[k].commercialTrack + `</td>
-                <td>` + saapumisaika.toUTCString().slice(17,22) + `</td>
-              </tr>
-              `)
-              iterator--;
-              console.log(iterator);
-          }
-        }
-      }
-    }
 
 
     }
 
-  )
+
+
+
+
+
+)
 
 
 
